d1213aee0bf541d0124eaaf26dd668c11a950871
hyperquack <david@pinkduckcompany.com>
2026-08-06 20:28:13 -0700

    styles

--- vex/static/js/screens/inventory.js
+++ vex/static/js/screens/inventory.js
@@ -71,7 +71,7 @@ class InventoryScreen {
                     </button>
                 </td>
                 <td class="asset-signals">
-                    ${Object.values(this.state.signals[this.state.account()]).filter(s => s.asset == a.name).length}
+                    <span class="signal-count">${Object.values(this.state.signals[this.state.account()]).filter(s => s.asset == a.name).length}</span>
                 </td>
                 <td class="asset-seen">${a.updated ? new Date(a.updated).toLocaleDateString() : '—'}</td>
             </tr>

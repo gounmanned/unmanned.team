d6596305ecd413357d8aa3b1c28bb348db6499fd
hyperquack <david@pinkduckcompany.com>
2026-09-19 15:26:22 -0400

    styles

--- cdn/js/components.js
+++ cdn/js/components.js
@@ -69,7 +69,7 @@ class SiteHeader extends HTMLElement {
           transition: background 0.15s;
         }
         .account-group:hover {
-          background: var(--line);
+          background: var(--surface-alt);
         }
         .account-name {
           font-size: 0.95rem;

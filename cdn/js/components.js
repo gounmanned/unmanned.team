7030c863c18560dcf6c00a599ee51e5644384524
hyperquack <david@pinkduckcompany.com>
2026-09-19 15:06:50 -0400

    styles

--- cdn/js/components.js
+++ cdn/js/components.js
@@ -15,7 +15,7 @@ class SiteHeader extends HTMLElement {
           right: 0;
           height: 52px;
           z-index: 10;
-          background: var(--surface));
+          background: var(--surface);
           border-bottom: 1px solid var(--line);
         }
         .header-container {
@@ -312,7 +312,7 @@ class SiteModal extends HTMLElement {
         width: 40%;
         max-height: 90vh;
         flex-direction: column;
-        background-color: var(--surface));
+        background-color: var(--surface);
         z-index: 1002;
         opacity: 0;
         pointer-events: none;
@@ -376,7 +376,7 @@ class SiteSidebar extends HTMLElement {
         display: flex;
         justify-content: center;
         flex-direction: column;
-        background-color: var(--surface));
+        background-color: var(--surface);
         box-shadow: 0 0 5px rgba(224, 18, 128, 0.5), 0 0 40px rgba(224, 18, 128, 0.2);
         z-index: 1001;
         transform: translateX(100%);
@@ -444,7 +444,7 @@ class SiteRollup extends HTMLElement {
           height: 90%;
           display: flex;
           flex-direction: column;
-          background-color: var(--surface));
+          background-color: var(--surface);
           border-top: 1px solid var(--line);
           box-shadow: 0 0 5px rgba(224, 18, 128, 0.5), 0 0 40px rgba(224, 18, 128, 0.2);
           border-radius: 12px 12px 0 0;
